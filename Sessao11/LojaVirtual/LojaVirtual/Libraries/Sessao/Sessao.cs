18bd63b88b718c06ca46d6634062b088d1cc3ccd
Elizabete <55201193+GitBete@users.noreply.github.com>
2020-03-16 22:30:04 -0300

    LoginColaboradorFinalizado

---
+++ Sessao11/LojaVirtual/LojaVirtual/Libraries/Sessao/Sessao.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Sessao
+{
+    public class Sessao
+    {
+        //incluir o servico: AddHttpContextAccessor
+        private IHttpContextAccessor _context;
+
+        public Sessao(IHttpContextAccessor context)
+        {
+            _context = context;
+        }
+        //CRUD - Cadsatrar/Atualizar/Consultar/Remover - RemoverTodos/Exist
+
+        public void Cadastrar(string key,string valor)
+        {
+            _context.HttpContext.Session.SetString(key, valor);
+        }
+
+        public void Atualizar(string key,string valor)
+        {
+            if (Existe(key))
+            {
+                _context.HttpContext.Session.Remove(key);
+            }
+            _context.HttpContext.Session.SetString(key, valor);
+        }
+
+        public void Remover(string key)
+        {
+            _context.HttpContext.Session.Remove(key);
+        }
+
+        public void RemoverTodos()
+        {
+            _context.HttpContext.Session.Clear();
+        }
+
+        public String Consultar(string key)
+        {
+           return  _context.HttpContext.Session.GetString(key);
+        }
+
+        public bool Existe(string key)
+        {
+           if ( _context.HttpContext.Session.GetString(key) ==null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Sessao11/LojaVirtual/LojaVirtual/Repositories/ColaboradorRepository.cs
@@ -0,0 +1,56 @@
+﻿using LojaVirtual.Database;
+using LojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Repositories
+{
+    public class ColaboradorRepository : IColaboradorRepository
+    {
+        private LojaVirtualContext _banco;
+
+        public ColaboradorRepository(LojaVirtualContext banco)
+        {
+            _banco = banco;
+        }
+
+        public void Cadastrar(Colaborador colaborador)
+        {
+            _banco.Add(colaborador);
+            _banco.SaveChanges();
+        }
+
+        public void Atualizar(Colaborador colaborador)
+        {
+            _banco.Update(colaborador);
+            _banco.SaveChanges();
+        }
+
+      
+
+        public void Excluir(int Id)
+        {
+            Colaborador colaborador = ObterColaborador(Id);
+            _banco.Remove(colaborador);
+            _banco.SaveChanges();
+        }
+
+        public Colaborador Login(string Email, string Senha)
+        {
+            Colaborador colaborador = _banco.Colaboradores.Where(m => m.Email == Email && m.Senha == Senha).FirstOrDefault();
+            return colaborador;
+        }
+
+        public Colaborador ObterColaborador(int Id)
+        {
+            return _banco.Colaboradores.Find(Id);
+        }
+
+        public IEnumerable<Colaborador> ObterTodosColaboradores()
+        {
+            return _banco.Colaboradores.ToList();
+        }
+    }
+}

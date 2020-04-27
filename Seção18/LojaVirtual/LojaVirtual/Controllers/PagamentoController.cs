051cc9251d9a440ff840566dc33ca4d8959a6982
GitBete <55201193+GitBete@users.noreply.github.com>
2020-04-27 00:46:57 -0300

    IndexPagamento

---
+++ Seção18/LojaVirtual/LojaVirtual/Controllers/PagamentoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+
+    public class PagamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
